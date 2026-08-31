3990c9f7f6733b5e2096922933b40b155811f1a8
vwtjk5pwrz-dev <vwtjk5pwrz@privaterelay.appleid.com>
2026-09-01 01:34:22 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='ninebot-suspension-pro-v4';
-const FILES=['./','./index.html','./style.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
